7f77ca2ae32c80cb966706820a5546d047491eec
ksdahmani10 <121550471+ksdahmani10@users.noreply.github.com>
2024-02-05 15:20:22 +0100

    Delete 10-loops.js

--- 10-loops.js
+++
@@ -1,8 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-  /* eslint-disable */
-  const end = [];
-  for (const value of array) {
-    end.push(appendString + value);
-  }
-  return end;
-}
