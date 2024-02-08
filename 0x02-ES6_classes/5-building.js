fe9429a92d2896323d9666ed2086a3d591636ab4
ksdahmani10 <121550471+ksdahmani10@users.noreply.github.com>
2024-02-08 10:40:18 +0100

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,30 +1,26 @@
-export default class Building() {
-  /* eslint-disable */
-  export default class Building() {
-  /* eslint-disable */
+/**
+ * Represents a class Building.
+ */
+export default class Building {
+  /**
+   * Creates a new Building instance.
+   *
+   * @param {Number} sqft - Square feet of the building.
+   */
   constructor(sqft) {
-    this._sqft = sqft;
-    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    if (
-      this.constructor !== Building
-      && typeof this.evacuationWarningMessage !== 'function'
-    ) {
-      throw Error(
-	'Class extending Building must override evacuationWarningMessage',
-      );
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
     }
-
-    this._sqft = sqft;
   }
 
-  // getter
   get sqft() {
     return this._sqft;
   }
 
-  // setter 
-  set sqft(newSqft) {
-    this._sqft = newSqft;
+  set sqft(value) {
+    this._sqft = value;
   }
 }
